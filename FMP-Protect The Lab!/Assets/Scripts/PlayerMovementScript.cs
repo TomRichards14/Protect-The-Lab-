ecefa8d2fc098311a7534d61b176255de406dddb
TomRichards14 <2richt38@solent.ac.uk>
2018-03-16 18:15:08 +0000

    Player movement code base
    Somewhat broken right now

---
+++ FMP-Protect The Lab!/Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour {
+
+    float PlayerSpeed;
+
+    Vector3 PlayerDirection;
+
+	// Use this for initialization
+	void Start ()
+    {
+        PlayerSpeed = 2.5f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetKey(KeyCode.W))
+        {
+            PlayerDirection = Vector3.forward;
+            PlayerMovement();
+        }
+	}
+
+    void PlayerMovement()
+    {
+        transform.position = PlayerDirection * PlayerSpeed;
+    }
+}
