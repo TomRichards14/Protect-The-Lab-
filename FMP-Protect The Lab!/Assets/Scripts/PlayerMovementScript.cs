e912648820d5180b4cc2879bb23763d3dc105b70
TomRichards14 <2richt38@solent.ac.uk>
2018-03-22 14:46:49 +0000

    Implemented movement
    Player rotation needs fixing and more directions need adding, but the base is there

--- FMP-Protect The Lab!/Assets/Scripts/PlayerMovementScript.cs
+++ FMP-Protect The Lab!/Assets/Scripts/PlayerMovementScript.cs
@@ -4,28 +4,46 @@ using UnityEngine;
 
 public class PlayerMovementScript : MonoBehaviour {
 
-    float PlayerSpeed;
+    public float PlayerMovementSpeed = 250f;
+    public float PlayerRotationSpeed = 1.0f;
 
-    Vector3 PlayerDirection;
+    public Vector3 PlayerDirection = Vector3.zero;
+    public Vector3 MousePosition;
+    public Vector3 RotatePlayerTo;
 
 	// Use this for initialization
 	void Start ()
     {
-        PlayerSpeed = 2.5f;
+        PlayerMovementSpeed = 2.5f;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		if (Input.GetKey(KeyCode.W))
+        CheckForMovementInput();
+        PlayerRotation();
+    }
+
+    void CheckForMovementInput()
+    {
+        if (Input.GetKey(KeyCode.W))
         {
             PlayerDirection = Vector3.forward;
             PlayerMovement();
         }
-	}
+    }
 
     void PlayerMovement()
     {
-        transform.position = PlayerDirection * PlayerSpeed;
+        transform.position += PlayerDirection * PlayerMovementSpeed * Time.deltaTime;
+    }
+
+    void PlayerRotation()
+    {
+        MousePosition = Input.mousePosition;
+        Debug.Log(MousePosition);
+        RotatePlayerTo = new Vector3(0.0f, MousePosition.y, 0.0f);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, RotatePlayerTo, PlayerRotationSpeed);
     }
 }
