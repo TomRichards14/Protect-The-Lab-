a0b30c8c540699e252e3ae56dc99a7fda1057e8c
TomRichards14 <2richt38@solent.ac.uk>
2018-04-17 18:05:36 +0100

    Begin shooting implementation

---
+++ FMP-Protect The Lab!/Assets/Scripts/EnemyManagerScript.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManagerScript : MonoBehaviour {
+
+    public GameObject NormalEnemyPrefab;
+    public GameObject CoreEnemyPrefab;
+    public GameObject PlayerGameObject;
+
+    public float SpawnXPosition;
+    public float SpawnXVariation;
+    public float SpawnZPosition;
+    public float SpawnZVariation;
+
+    public int QuantityOfEnemiesInObjectPool = 125;
+    public int CurrentQuantityInWave;
+
+    List<GameObject> NormalEnemiesObjectPool;
+    List<GameObject> CoreEnemiesObjectPool;
+
+    public bool IsCurrentWaveOver;
+
+	// Use this for initialization
+	void Start ()
+    {
+        IsCurrentWaveOver = true;
+        NormalEnemiesObjectPool = new List<GameObject>();
+        CoreEnemiesObjectPool = new List<GameObject>();
+
+        //Object pool for the normal enemies that chase the player
+        for (int i = 0; i < QuantityOfEnemiesInObjectPool; i++)
+        {
+            GameObject obj = (GameObject)Instantiate(NormalEnemyPrefab);
+            obj.SetActive(false);
+            NormalEnemiesObjectPool.Add(obj);
+        }
+
+        //Object pool for the enemies that will only go for the core
+        for (int i = 0; i < QuantityOfEnemiesInObjectPool; i++)
+        {
+            GameObject obj = (GameObject)Instantiate(CoreEnemyPrefab);
+            obj.SetActive(false);
+            CoreEnemiesObjectPool.Add(obj);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ChooseNewSpawnPoint();
+		//if (IsCurrentWaveOver == true)
+        //{
+        //    SpawnXPosition = 0.0f;
+        //    ChooseNewSpawnPoint();
+        //}
+	}
+
+    void ChooseNewSpawnPoint()
+    {
+        //while ((SpawnXPosition > -16.0f) || (SpawnXPosition < 16.0f))
+        {
+            SpawnXPosition = Random.Range(-20, 20);
+        }
+        Debug.Log(SpawnXPosition);
+
+
+        SpawnZPosition = Random.Range(-30, 30);        
+    }
+}
---
+++ FMP-Protect The Lab!/Assets/Scripts/PlayerManagerScript.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManagerScript : MonoBehaviour {
+
+    public GameObject BulletPrefab;
+
+    private float PlayerMovementSpeed = 5f;
+    private float PlayerRotationSpeed = 250.0f;
+
+    private int QuantityOfBulletsInObjectPool = 25;
+
+    public Vector3 PlayerDirection = Vector3.zero;
+    public Vector3 MousePosition;
+    public Vector3 RotatePlayerTo;
+
+    List<GameObject> BulletObjectPool;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //Instantiating the object pool for the bullets
+        BulletObjectPool = new List<GameObject>();
+
+        //Adding the GameObjects to the object pool
+        for (int i = 0; i < QuantityOfBulletsInObjectPool; i++)
+        {
+            GameObject obj = (GameObject)Instantiate(BulletPrefab);
+            obj.SetActive(false);
+            BulletObjectPool.Add(obj);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        CheckForMovementInput();
+        if (Input.GetButtonDown("Fire1"))
+        {
+            PlayerShooting();
+        }        
+    }
+
+    public void CheckForMovementInput()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(PlayerMovementSpeed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(-PlayerMovementSpeed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(-Vector3.up * PlayerRotationSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * PlayerRotationSpeed * Time.deltaTime);
+        }
+    }
+
+    public void PlayerShooting()
+    {
+        Vector3 MousePosInWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+}
