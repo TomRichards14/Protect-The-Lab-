b1da31d1713a621b52e6ff7d1cddaad7fcbe8573
TomRichards14 <2richt38@solent.ac.uk>
2018-04-19 17:07:44 +0100

    Added FSM for AI

---
+++ FMP-Protect The Lab!/Assets/Scripts/EnemyActionsManagerScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public static class ConstantTags
+{
+    public const string PlayerTag = "Player";
+    public const string CoreTag = "Core";
+}
+
+public class EnemyActionsManagerScript : MonoBehaviour {
+
+    public int MaximumHealth = 100;
+    public int CurrentHealth;
+    public const int Damage = 5;
+
+    public const float MeleeRange = 1.0f;
+    public const float CollectRangee = 1.0f;
+
+    public bool IsAlive;
+
+    public Vector3 SpawnPosition;
+
+    private NavMeshAgent EnemyAgent;
+
+	// Use this for initialization
+	void Start ()
+    {
+        EnemyAgent = GetComponent<NavMeshAgent>();
+        SpawnPosition = transform.position;
+        CurrentHealth = MaximumHealth;
+        IsAlive = true;
+	}
+	
+	public void MoveTowardsPlayer(GameObject PlayerGameObject)
+    {
+        EnemyAgent.destination = PlayerGameObject.transform.position;
+    }
+
+    public void MoveTowardsCorePiece(GameObject CorePieceGameObject)
+    {
+        EnemyAgent.destination = CorePieceGameObject.transform.position;
+    }
+
+    public void AttackPlayer(GameObject PlayerGameObject)
+    {
+        if ((PlayerGameObject.CompareTag(ConstantTags.PlayerTag)) && (Vector3.Distance(transform.position, PlayerGameObject.transform.position) < MeleeRange))
+        {
+
+        }
+    }
+
+    public void TakeDamage(int DamageTaken)
+    {
+        CurrentHealth -= DamageTaken;
+        if (CurrentHealth <= 0)
+        {
+            EnemyDeath();
+        }
+    }
+
+    public void EnemyDeath()
+    {
+        IsAlive = false;
+    }
+}
